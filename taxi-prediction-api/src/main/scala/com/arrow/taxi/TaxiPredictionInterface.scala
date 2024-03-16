734339309cd18566d792de1962e672777e519d2e
Fletcher B Sims <fletcher.sims@gmail.com>
2024-03-16 09:31:12 -0400

    update the interface

--- taxi-prediction-api/src/main/scala/com/arrow/taxi/TaxiPredictionInterface.scala
+++ taxi-prediction-api/src/main/scala/com/arrow/taxi/TaxiPredictionInterface.scala
@@ -1,5 +1,8 @@
 package com.arrow.taxi
 
-trait TaxiPredictionInterface {
+import org.apache.spark.sql.Dataset
 
+trait TaxiPredictionInterface {
+  this: TaxiPredictionService =>
+  def reader(path: String, format: String): Dataset[_]
 }
