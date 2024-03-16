96869aa0b2b1c0ae4e971e7e7d07ad57650ca881
Fletcher B Sims <fletcher.sims@gmail.com>
2024-03-16 09:29:56 -0400

    add in reader function'

--- taxi-prediction-api/src/main/scala/com/arrow/taxi/TaxiPredictionDriver.scala
+++ taxi-prediction-api/src/main/scala/com/arrow/taxi/TaxiPredictionDriver.scala
@@ -2,10 +2,10 @@ package com.arrow.taxi
 
 import com.arrow.utils.SparkService
 
-object TaxiPredictionDriver extends SparkService {
+object TaxiPredictionDriver extends SparkService with TaxiPredictionService {
   def main(args: Array[String]) = {
     import spark.implicits._
-    val taxi = spark.read.parquet("./data/taxi/yellow_tripdata_2023-01.parquet")
+    val taxi = reader("./data/taxi/yellow_tripdata_2023-01.parquet", "parquet")
     taxi.printSchema()
     taxi.show(333, false)
   }
--- taxi-prediction-api/src/main/scala/com/arrow/taxi/TaxiPredictionService.scala
+++ taxi-prediction-api/src/main/scala/com/arrow/taxi/TaxiPredictionService.scala
@@ -1,5 +1,17 @@
 package com.arrow.taxi
 
-trait TaxiPredictionService {
+import com.arrow.utils.SparkService
+import org.apache.spark.sql.Dataset
 
+trait TaxiPredictionService extends SparkService {
+  def reader(path: String, format: String): Dataset[_] = {
+    val supportedFormats = Set("json", "csv", "parquet")
+    if (!supportedFormats.contains(format)) {
+      throw new IllegalArgumentException(s"Unsupported format: $format")
+    }
+
+    val reader = spark.read.format(format)
+    val data = reader.load(path)
+    data
+  }
 }
