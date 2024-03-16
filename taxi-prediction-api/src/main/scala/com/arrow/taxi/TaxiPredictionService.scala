e735c8934e2fb0a5adf17fc9bbbfcbf456a944f8
Fletcher B Sims <fletcher.sims@gmail.com>
2024-03-16 09:40:27 -0400

    fix service

--- taxi-prediction-api/src/main/scala/com/arrow/taxi/TaxiPredictionService.scala
+++ taxi-prediction-api/src/main/scala/com/arrow/taxi/TaxiPredictionService.scala
@@ -3,7 +3,7 @@ package com.arrow.taxi
 import com.arrow.utils.SparkService
 import org.apache.spark.sql.Dataset
 
-trait TaxiPredictionService extends SparkService {
+trait TaxiPredictionService extends SparkService with TaxiPredictionInterface {
   def reader(path: String, format: String): Dataset[_] = {
     val supportedFormats = Set("json", "csv", "parquet")
     if (!supportedFormats.contains(format)) {
