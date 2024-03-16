811183f1a4af7c4562a639cee558502647b56c2b
Fletcher B Sims <fletcher.sims@gmail.com>
2024-03-16 13:20:34 -0400

    reformat files

--- taxi-prediction-api/src/main/scala/com/arrow/model/TaxiModel.scala
+++ taxi-prediction-api/src/main/scala/com/arrow/model/TaxiModel.scala
@@ -3,25 +3,26 @@ package com.arrow.model
 import java.sql.Timestamp
 
 import org.apache.spark.sql.{Encoder, Encoders}
-case class TaxiModel (
-    VendorID: Long,
-    tpep_pickup_datetime: Option[Timestamp],
-    tpep_dropoff_datetime: Option[Timestamp],
-    passenger_count: Option[Double],
-    trip_distance: Option[Double],
-    RatecodeID: Option[Double],
-    store_and_fwd_flag: Option[String],
-    PULocationID: Option[Long],
-    DOLocationID: Option[Long],
-    payment_type: Option[Long],
-    fare_amount: Option[Double],
-    extra: Option[Double],
-    mta_tax: Option[Double],
-    tip_amount: Option[Double],
-    tolls_amount: Option[Double],
-    improvement_surcharge: Option[Double],
-    total_amount: Option[Double],
-    congestion_surcharge: Option[Double],
-    airport_fee: Option[Double]
-                     )
+
+case class TaxiModel(
+                      VendorID: Long,
+                      tpep_pickup_datetime: Option[Timestamp],
+                      tpep_dropoff_datetime: Option[Timestamp],
+                      passenger_count: Option[Double],
+                      trip_distance: Option[Double],
+                      RatecodeID: Option[Double],
+                      store_and_fwd_flag: Option[String],
+                      PULocationID: Option[Long],
+                      DOLocationID: Option[Long],
+                      payment_type: Option[Long],
+                      fare_amount: Option[Double],
+                      extra: Option[Double],
+                      mta_tax: Option[Double],
+                      tip_amount: Option[Double],
+                      tolls_amount: Option[Double],
+                      improvement_surcharge: Option[Double],
+                      total_amount: Option[Double],
+                      congestion_surcharge: Option[Double],
+                      airport_fee: Option[Double]
+                    )
 
--- taxi-prediction-api/src/main/scala/com/arrow/taxi/TaxiPredictionInterface.scala
+++ taxi-prediction-api/src/main/scala/com/arrow/taxi/TaxiPredictionInterface.scala
@@ -6,5 +6,5 @@ import scala.reflect.runtime.universe.TypeTag
 
 trait TaxiPredictionInterface {
   this: TaxiPredictionService =>
-  def reader[T <: Product: TypeTag](path: String, format: String)(implicit encoder: Encoder[T]): Dataset[T]
+  def reader[T <: Product : TypeTag](path: String, format: String)(implicit encoder: Encoder[T]): Dataset[T]
 }
